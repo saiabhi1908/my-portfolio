bd1d178c84e9965be8beee0629e70bd81e783074
saiabhi1908 <praveenakolli08@gmail.com>
2024-11-14 15:19:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* 
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
@@ -73,4 +74,5 @@ app.get('/check-emails', (req, res) => {
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
+}); 
+*/
