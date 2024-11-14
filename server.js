177fe771340186fb060aca0e92e8bca3086d9c8b
saiabhi1908 <praveenakolli08@gmail.com>
2024-11-14 13:56:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'https://saiabhi1908.github.io' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
